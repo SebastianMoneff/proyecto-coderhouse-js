8274361ace7eb81c46803d11d19a8ec847cb68ca
Sebastian Moneff <sebastianmoneff@gmail.com>
2023-07-27 20:54:24 -0300

    json y detalles en fetch

---
+++ js/carrito.js
@@ -0,0 +1,86 @@
+let comicsEnCarrito = localStorage.getItem("comics-en-carrito")
+comicsEnCarrito = JSON.parse(comicsEnCarrito);
+
+const carritoVacio = document.querySelector("#carrito-vacio");
+const carritoProductos = document.querySelector("#carrito-productos");
+const botonesCarritoFunciones = document.querySelector("#botones-carrito-funciones");
+const carritoComprado = document.querySelector("#carrito-comprado");
+let botonesEliminar = document.querySelectorAll(".carrito-comic-eliminar")
+const botonVaciarCarroDeCompras = document.querySelector("#carrito-boton-vaciar")
+const carritoTotal = document.querySelector("#total")
+const botonComprar = document.querySelector("botones-carrito-comprar")
+
+function agregarAlCarrito() {
+    if (agregarAlCarrito && agregarAlCarrito.length > 0) {
+        
+        carritoVacio.classList.add("disable");
+        carritoProductos.classList.remove("disable");
+        botonesCarritoFunciones.classList.remove("disable");
+        CarritoComprado.classList.add("disable");
+        
+        carritoProductos.innerHTML = "";
+        
+        agregarAlCarrito.forEach(comic => { 
+        
+            const div = document.createElement("div");
+            div.classList.add("carrito-comic");
+            div.innerHTML = `
+                <img class="carrito-comic-imagen" src="${comic.imagen}" alt="${comic.nombre}">
+                <div class="carrito-comic-nombre">
+                    <small>Producto</small>
+                    <h3>${comic.nombre}</h3>
+                </div>
+                <div class="carrito-comic-cantidad">
+                    <small>Cantidad</small>
+                    <p>${comic.cantidad}</p>
+                </div>
+                <div class="carrito-comic-precio">
+                    <small>Precio</small>
+                    <p>${comic.precio}</p>
+                </div>
+                <div class="carrrito-comic-total">
+                    <small>Total</small>
+                    <p>${comic.precio * comic.cantidad}</p>
+                </div>
+                <Button id="${comic.id}" class="carrito-comic-eliminar">Eliminar</Button>
+            `;
+
+            carritoProductos.append(div);
+        })
+
+    } else {
+        carritoVacio.classList.remove("disable");
+        carritoProductos.classList.add("disable");
+        botonesCarritoFunciones.classList.add("disable");
+        CarritoComprado.classList.add("disable");
+    }
+
+    cargarProductosEliminar();
+}
+
+cargarProductosCarrito();
+
+function actializarBotonesEliminar() {
+    botonesEliminar = document.querySelectorAll(".carrito-comic-eliminar");
+    
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click", eliminarDelCarrito);
+    });
+}
+
+function actializarBotonesEliminar(e) {
+    const idBoton = e.currentTarget.id;
+    const index = agregarAlCarrito.splice(index, 1);
+    agregarAlCarrito.splice(index, 1);
+    cargarProductosEliminar();
+    localStorage.setItem("productos en carrito", JSON.stringify(agregarAlCarrito));
+
+}
+
+function vaciarCarroDeCompras() {
+    agregarAlCarrito.length = 0;
+    localStorage.setItem("productos en carrito", JSON.stringify(comicsEnCarrito));
+    agregarAlCarrito();
+}
+
+
--- js/main.js
+++ js/main.js
@@ -1,77 +1,143 @@
-/*
-Proyecto de JavaScript
-Inicio de codigo
-*/
-
-
 const comics = [
   {
   id: "comic-batman", 
   nombre:"Batman: The Dark Knight Returns", 
   precio: 10000, 
-  imagen:"./images/americanos/batman", 
+  imagen:"./images/americanos/batman.jpg", 
   categoria:{nombre:"Americanos", id:"americanos",}
 },
 {
   id: "comic-vdevendetta", 
   nombre:"V de Vendetta", 
   precio: 10000, 
-  imagen:"./images/americanos/batman", 
+  imagen:"./images/americanos/vdevendetta.jpg", 
   categoria:{nombre:"Americanos", id:"americanos",}
 },
 {
   id: "comic-hellboy", 
   nombre:"Hellboy", 
   precio: 10000, 
-  imagen:"./images/americanos/batman", 
+  imagen:"./images/americanos/hellboy.jpg", 
   categoria:{nombre:"Americanos", id:"americanos",}
 },
 {
   id: "comic-watchmen", 
   nombre:"Watchmen", 
   precio: 10000, 
-  imagen:"./images/americanos/watchmen", 
+  imagen:"./images/americanos/watchmen.jpg", 
   categoria:{nombre:"Americanos", id:"americanos",}
 },
 {
   id: "comic-blacksad", 
   nombre:"Blacksad", 
   precio: 10000, 
-  imagen:"./images/independientes/blacksad", 
+  imagen:"./images/independientes/blacksad.jpg", 
   categoria:{nombre:"Independientes", id:"independientes",}
 },
 {
   id: "comic-radiantblack", 
   nombre:"Radiant Black", 
   precio: 10000, 
-  imagen:"./images/independientes/radiantblack", 
+  imagen:"./images/independientes/radiantblack.jpg", 
   categoria:{nombre:"Independientes", id:"independientes",}
 },
 {
   id: "comic-akira", 
   nombre:"Akira", 
   precio: 10000, 
-  imagen:"./images/mangas/akira", 
+  imagen:"./images/mangas/akira.jpg", 
   categoria:{nombre:"Mangas", id:"mangas",}
 },
 ];
 
-const productosIndex = document.querySelector("#productos-index");
+/*Elementos del DOM*/
+let productos = [];
 
-function cargarComics () {
-  comics.forEach (producto => {
-    const div = document.createElement("div");
-    div.classList.add("producto-index");
+const productosIndex = document.querySelector("#productos-index");
+const botonCategoria = document.querySelectorAll(".boton-categoria");
+const tituloIndex = document.querySelector("#titulo-index");
+let botonComprar = document.querySelectorAll(".comic-index-comprar");
+const numero = document.querySelector(".numero");
+
+
+function cargarComics (comicsSeleccionados) {
+
+  productosIndex.innerHTML = "";
+  comicsSeleccionados.forEach (comic => {
+
+    const div = document.createElement ("div");
+    div.classList.add("comic-index");
     div.innerHTML = `
-    <images class="producto-index-imagen" src="${producto.imagen}" alt="${producto.nombre}">
-    <div class="producto-index-detalles">
-        <h3 class="producto-index-nombre">${producto.nombre}</h3>
-        <p class="producto-index-precio">$${producto.precio}</p>
-        <button id="${producto.id}" class="producto-index-comprar">Comprar</button>
-    </div>
+      <img class="comic-index-imagen" src="${comic.imagen}" alt="${comic.nombre}">
+      <div class="comic-index-detalles">
+        <h3 class="comic-index-nombre">${comic.nombre}</h3>
+        <p class="comic-index-precio">$${comic.precio}</p>
+        <button id="${comic.id}" class="comic-index-comprar">Comprar</button>
+      </div>
     `;
-    productosIndex.append(div);
+    
+      productosIndex.append(div);
   })
+  botonComprarActualizado();
 }
-cargarComics ();
+
+
+botonCategoria.forEach (boton => {
+  boton.addEventListener ("click", (e)=> {
+
+    botonCategoria.forEach(boton => boton.classList.remove("active"));
+    e.currentTarget.classList.add("active");
+
+    if (e.currentTarget.id != "todos") {
+      const botonCategoria = comics.find(comic => comic.categoria.id === e.currentTarget.id)
+      tituloIndex.innerText = comicsBoton.categoria.nombre;
+      const comicsBoton = comics.filter(comic => comic.categoria.id === e.currentTarget.id)
+      cargarComics (comicsBoton);
+    } else {
+      tituloIndex.innerText = "Todos nuestros comics:"
+      cargarComics(comics);
+    }
+
+  })
+});
+
+function botonComprarUsado() {
+  botonComprar = document.querySelectorAll(".comic-index-comprar");
+  
+  botonComprar.forEach(boton => {
+    boton.addEventListener("click", agregarAlCarrito);
+  });
+}
+
+let comicsEnCarrito;
+
+let comicsEnCarritoLS = localStorage.getItem("comics-en-carrito");
+
+if(comicsEnCarritoLS) {
+  comicsEnCarrito = JSON.parse(comicsEnCarritoLS);
+  actualizarNumero();
+} else {
+const comicsEnCarrito = [];
+}
+
+function agregarAlCarrito (e) {
+
+  const idBoton = e.currentTarget.id;
+  const comicAgregado = comics.find(comic => comic.id === idBoton);
+  if (comicsEnCarrito.some(comic => comic.id === idBoton)) {
+    const index = comicsEnCarrito.findIndex(comic => comic.id === idBoton)
+    comicsEnCarrito[index].cantidad++;
+  } else {
+    comicAgregado.cantidad = 1;
+    comicsEnCarrito.push(comicAgregado);
+  }
+  actualizarNumero();
+
+  localStorage.setItem("comics-en-carrito", JSON.stringify(comicsEnCarrito))
+}
+
+function actualizarNumero() {
+  let numeroNuevo = comicsEnCarrito.reduce((acc, comic) => acc + comic.cantidad, 0);
+  numero.innerText = numeroNuevo;
+}
 
